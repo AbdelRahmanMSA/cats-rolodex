36c9c79f8726b970e8191068b13c50616d6ec481
AbdelRahmanMSA <abdelrahman.msa@gmail.com>
2020-01-28 22:38:35 +0200

    Updates

---
+++ precache-manifest.7e21154c5192339cd52991b643925b24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72fb316a02e7ef285cba37be792e5566",
+    "url": "/cats-rolodex/index.html"
+  },
+  {
+    "revision": "5e126d1dd6c02c00cb23",
+    "url": "/cats-rolodex/static/css/main.f85e0724.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/cats-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cats-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e126d1dd6c02c00cb23",
+    "url": "/cats-rolodex/static/js/main.c99c35c7.chunk.js"
+  },
+  {
+    "revision": "badcca6ea9a0f4eefa9b",
+    "url": "/cats-rolodex/static/js/runtime-main.947174fc.js"
+  }
+]);

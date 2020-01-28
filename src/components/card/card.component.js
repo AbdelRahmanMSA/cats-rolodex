66c350671d62da3e3893901a64fb1170c176bf3b
AbdelRahmanMSA <abdelrahman.msa@gmail.com>
2020-01-28 22:40:25 +0200

    cats-rolodex init

---
+++ src/components/card/card.component.js
@@ -0,0 +1,17 @@
+import "./card.styles.css";
+import React from "react";
+
+const Card = props => {
+  return (
+    <div className="card-container">
+      <img
+        alt="monster"
+        src={`https://robohash.org/${props.monster.id}?set=set4&size=180x180`}
+      />
+      <h2>{props.monster.name}</h2>
+      <p>{props.monster.email}</p>
+    </div>
+  );
+};
+
+export default Card;
